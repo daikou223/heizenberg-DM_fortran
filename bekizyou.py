badc4c84691214a45069b1f85a1a0a1405ea07a8
daikou1928 <daikou1928@gmail.com>
2025-07-12 07:32:14 +0900

    firstcommit

--- bekizyou.py
+++ bekizyou.py
@@ -6,20 +6,18 @@ from scipy.sparse.linalg import spsolve
 def koyuuti(hamil,n):
 
     I = identity(n, format='csc')
-    x = np.ones(n)
+    x = np.array([1+1j]*n)
 
     row_sums = np.array(hamil.sum(axis=1)).flatten()
     sigma = max(map(abs,row_sums))
     hamil = -hamil + (sigma+1)*I
-    print(hamil)
-    print(x[0] is x[1] if len(x)>1 else "")
-    for _ in range(50):
+    i = 0
+    while 1:
         y = x.copy()
         x = hamil@x
         x = x / np.linalg.norm(x)
-        print(x,y)
-        if np.linalg.norm(x - y) < 1e-6 or np.linalg.norm(x + y) < 1e-6:
-            break
-    print(x is y)
+        if (np.linalg.norm(x - y) < 1e-6 or np.linalg.norm(x + y) < 1e-6) and i>30:
+             break
+        i += 1
     val = ((hamil @ x)[0]/x[0] - sigma - 1)*-1
     return val
--- v5.py
+++ v5.py
@@ -19,11 +19,11 @@ class Site():
         return f"Site(id={self.id},bond = {self.bond},mod = {self.mod})"
 #定数***********************************
 bond_mod_J = 1
-bond_mod_D = 0.1
+bond_mod_D = -0.1
 accuracy = 1e-2 #精度
 Sites = []
 SITE_NUM = 0
-inputFile = "N=3_test"
+inputFile = "N=21"
 #変数********************************
 #Stateの配列
 States = []
